e1e359f877615daf20f060306538b378e59c8131
bariabdul <abdul.bari.1@outlook.com>
2018-03-21 19:17:52 -0700

    initial commit

---
+++ pixel-city/Controller/PopVC.swift
@@ -0,0 +1,58 @@
+//
+//  PopVC.swift
+//  pixel-city
+//
+//  Created by Bari Abdul on 2/8/18.
+//  Copyright © 2018 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popImageView.image = passedImage
+
+        addDoubleTap()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initData(forImage image: UIImage) {
+       self.passedImage = image
+    }
+    
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
